f299f15fe87239333c2d5ffd4fc6c31353f2f3b4
cossio <j.cossio.diaz@gmail.com>
2022-01-30 02:00:35 +0100

    allow initial vm for pcd chain add more comments

--- src/advpcd.jl
+++ src/advpcd.jl
@@ -6,24 +6,31 @@ Trains the RBM on data using Persistent Contrastive divergence with constraints.
 function advpcd!(rbm::RBMs.RBM, data::AbstractArray;
     batchsize::Int = 1,
     epochs::Int = 1,
-    optimizer = Flux.ADAM(), # optimizer algorithm
-    history::ValueHistories.MVHistory = ValueHistories.MVHistory(), # stores training log
-    wts = nothing, # data point weights
-    steps::Int = 1, # Monte Carlo steps to update fantasy particles
-    q::Union{AbstractArray, Nothing} = nothing, # 1st-order constraint
-    Q::Union{AbstractArray, Nothing} = nothing, # 2nd-order constraint
-    λQ::Real = Inf, # 2nd-order penalty
-    μc::CartesianIndices = CartesianIndices(size(rbm.hidden)) # constrained hidden units
+    # optimizer algorithm
+    optimizer = Flux.ADAM(),
+    # stores training log
+    history::ValueHistories.MVHistory = ValueHistories.MVHistory(),
+    # data point weights
+    wts = nothing,
+    # Monte Carlo steps to update fantasy particles
+    steps::Int = 1,
+    # initial state of fantasy chains (default: sample visible layer)
+    vm::AbstractArray = transfer_sample(rbm.visible, falses(size(rbm.visible)..., batchsize)),
+    # 1st-order constraint
+    q::Union{AbstractArray, Nothing} = nothing,
+    # 2nd-order constraint
+    Q::Union{AbstractArray, Nothing} = nothing,
+    # 2nd-order penalty
+    λQ::Real = Inf,
+    # indices of constrained hidden units
+    μc::CartesianIndices = CartesianIndices(size(rbm.hidden))
 )
     @assert size(data) == (size(rbm.visible)..., size(data)[end])
     @assert isnothing(wts) || _nobs(data) == _nobs(wts)
-
+    # sufficient statistics for visible layer (e.g., <v>_d)
     stats = RBMs.sufficient_statistics(rbm.visible, data; wts)
-
-    # initialize fantasy chains by sampling visible layer
-    vm = RBMs.transfer_sample(rbm.visible, falses(size(rbm.visible)..., batchsize))
-
-    ℐ = CartesianIndices(axes(CartesianIndices(size(rbm.visible)))..., axes(μc)...)
+    # indices of visible dimensions
+    𝒱 = CartesianIndices(size(rbm.visible))
 
     for epoch in 1:epochs
         batches = RBMs.minibatches(data, wts; batchsize = batchsize)
@@ -31,19 +38,20 @@ function advpcd!(rbm::RBMs.RBM, data::AbstractArray;
             vm = RBMs.sample_v_from_v(rbm, vm; steps = steps)
             ∂ = RBMs.∂contrastive_divergence(rbm, vd, vm; wd, stats)
             if λQ == Inf
-                ∂project!(view(∂.w, ℐ), q, Q)
+                ∂project!(view(∂.w, 𝒱, μc), q, Q)
             else
-                ∂.w .+= λQ .* ∂wQw(view(rbm.w, ℐ), Q)
+                ∂.w .+= λQ .* ∂wQw(view(rbm.w, 𝒱, μc), Q)
             end
             RBMs.update!(optimizer, rbm, ∂)
             push!(history, :∂, RBMs.gradnorms(∂))
         end
-        project!(view(rbm.w, ℐ), q)
+        project!(view(rbm.w, 𝒱, μc), q)
 
-        lpl = RBMs.wmean(log_pseudolikelihood(rbm, data); wts)
+        lpl = RBMs.wmean(RBMs.log_pseudolikelihood(rbm, data); wts)
         push!(history, :lpl, lpl)
         push!(history, :epoch, epoch)
         push!(history, :Δt, Δt)
+        push!(history, :vm, copy(vm))
 
         Δt_ = round(Δt, digits=2)
         lpl_ = round(lpl, digits=2)
