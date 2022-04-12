8556182dd2a9f09a85e10137cb7f6e88dcfed63c
cossio <j.cossio.diaz@gmail.com>
2022-04-12 18:57:28 +0200

    advpcd test constraining subset of hidden units

--- test/advpcd.jl
+++ test/advpcd.jl
@@ -11,4 +11,12 @@ using AdvRBMs: advpcd!
     data = bitrand(5,2,128)
     advpcd!(rbm, data; q, steps=1, epochs=1, batchsize=32)
     @test norm(inputs_v_to_h(rbm, q)) < 1e-10
+
+    rbm = BinaryRBM(randn(5,2), randn(3), randn(5,2,3))
+    q = randn(5,2)
+    data = bitrand(5,2,128)
+    ℋ = CartesianIndices((2:3,))
+    advpcd!(rbm, data; q, ℋ, steps=1, epochs=1, batchsize=32)
+    @test norm(inputs_v_to_h(rbm, q)[ℋ]) < 1e-10
+    @test norm(inputs_v_to_h(rbm, q)[CartesianIndices(1:1)]) > 1e-5
 end
