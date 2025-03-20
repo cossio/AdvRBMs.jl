f9ff9b7aa2c305b8514b6ccfb20b1152aa94c0cf
cossio <cossio@users.noreply.github.com>
2025-03-20 14:02:45 +0100

    Centered, Std

--- src/AdvRBMs.jl
+++ src/AdvRBMs.jl
@@ -6,10 +6,11 @@ using LinearAlgebra: sylvester, dot, Diagonal, eigen, pinv
 using Statistics: mean
 using FillArrays: Zeros, Falses
 using Optimisers: AbstractRule, setup, update!, Adam
-using CenteredRBMs: CenteredRBM, grad2ave, center_hidden!, center_from_data!
-using StandardizedRestrictedBoltzmannMachines: StandardizedRBM, standardize_visible_from_data!,
+using RestrictedBoltzmannMachines: StandardizedRBM, standardize_visible_from_data!,
     standardize_hidden_from_v!, rescale_hidden_activations!
-using RestrictedBoltzmannMachines: RBM, AbstractLayer, Binary, Spin, Potts,
+using RestrictedBoltzmannMachines: RBM
+using RestrictedBoltzmannMachines: CenteredRBM, grad2ave, center_hidden!, center_from_data!
+using RestrictedBoltzmannMachines: AbstractLayer, Binary, Spin, Potts,
     inputs_h_from_v, sample_v_from_v,
     ∂free_energy, ∂regularize!, batchmean, total_meanvar_from_inputs,
     sample_from_inputs, moments_from_samples, infinite_minibatches,
